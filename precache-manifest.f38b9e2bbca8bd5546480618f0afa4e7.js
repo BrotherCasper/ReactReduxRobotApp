1113bb44ec05cbc01032a191c28f8a5fdb17ec36
David <davidcasper7@gmail.com>
2019-01-06 18:23:15 +0200

    Updates

---
+++ precache-manifest.f38b9e2bbca8bd5546480618f0afa4e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactReduxRobotApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b522e594d59fa696f769",
+    "url": "/ReactReduxRobotApp/static/js/runtime~main.b522e594.js"
+  },
+  {
+    "revision": "996ab2eb3eae00f81d92",
+    "url": "/ReactReduxRobotApp/static/js/main.996ab2eb.chunk.js"
+  },
+  {
+    "revision": "75ac861775bb9627230e",
+    "url": "/ReactReduxRobotApp/static/js/1.75ac8617.chunk.js"
+  },
+  {
+    "revision": "996ab2eb3eae00f81d92",
+    "url": "/ReactReduxRobotApp/static/css/main.0e6de4f8.chunk.css"
+  },
+  {
+    "revision": "75ac861775bb9627230e",
+    "url": "/ReactReduxRobotApp/static/css/1.4b972714.chunk.css"
+  },
+  {
+    "revision": "67c476be39d5cc101d6b0e67005e76ff",
+    "url": "/ReactReduxRobotApp/index.html"
+  }
+];
