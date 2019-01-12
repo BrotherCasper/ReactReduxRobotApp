cb75ab88a5c1361e6cd36a87d025cca1ec39cf93
David <davidcasper7@gmail.com>
2019-01-12 15:37:42 +0200

    Added more test for to components and reducers

---
+++ src/components/MainPage.js
@@ -0,0 +1,45 @@
+import React ,{Component} from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+import './MainPage.css';
+import ErrorBoundry from '../components/ErrorBoundry';
+import Header from '../components/Header';
+
+class MainPage extends Component{
+    
+    componentDidMount(){
+        this.props.onRequestRobots();
+    }
+
+    isPending = (isPending) =>{
+        if(isPending){
+            return true;
+        }
+        return false;
+    }
+    
+    filteredRobots = () => {
+    return this.props.robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(this.props.searchField.toLowerCase()); 
+    })}
+    render(){
+        const {onSearchChange,isPending} =this.props;       
+        
+        return this.isPending(isPending) ?
+            (<h1>Loading</h1>):     
+        (
+                <div className = 'tc'>
+                    <Header/>
+                    <SearchBox searchChange = {onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundry>
+                    <CardList robots = {this.filteredRobots()}/>
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+        )  
+    }
+}
+
+export default MainPage;
